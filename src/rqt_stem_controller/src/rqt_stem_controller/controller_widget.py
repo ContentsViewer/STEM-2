c14ce34a54f08bde0b6b4b3ed083b449e908d1dd
IOE <fivetwothreesix@gmail.com>
2020-08-23 00:38:19 +0900

    developing rqt

--- src/rqt_stem_controller/src/rqt_stem_controller/controller_widget.py
+++ src/rqt_stem_controller/src/rqt_stem_controller/controller_widget.py
@@ -3,6 +3,8 @@ import os
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 from python_qt_binding.QtWidgets import QPushButton
+from python_qt_binding.QtWidgets import QListWidgetItem
+from python_qt_binding.QtGui import QIcon
 
 from qt_gui.ros_package_helper import get_package_path
 
@@ -14,22 +16,46 @@ class ControllerWidget(QWidget):
         ui_file = os.path.join(package_path, 'share', 'rqt_stem_controller', 'resource', 'STEMController.ui')
         loadUi(ui_file, self)
 
-        # btn1 = QPushButton("Button 1", self)
-        # btn1.move(30, 50)
-        # btn1.clicked.connect(self.buttonClicked)  
+        self.is_pressed_supervise_button = False
+        self.supervise_button.pressed.connect(self.supervise_button_pressed)
+        self.supervise_button.released.connect(self.supervise_button_released)
 
-    def keyPressEvent(self, e):
+        self.state_name_combo_box.setEditable(True)
+        self.state_name_remove_button.setIcon(QIcon.fromTheme('list-remove'))
+        self.state2widgets_mapper = {}
+        self.add_state('item')
+        self.add_state('item')
 
-        # # エスケープキーを押すと画面が閉じる
-        # if e.key() == Qt.Key_Escape:
-        #     self._widget.shutdown()
+    def supervise_button_pressed(self):
+        self.is_pressed_supervise_button = True
+
+    def supervise_button_released(self):
+        self.is_pressed_supervise_button = False
+
+    def add_state(self, state_name):
+        if state_name in self.state2widgets_mapper:
+            return
         
-        print(e.key())
-        self.supervised_state_name = e.key()
+        item = QListWidgetItem(state_name)
+        self.state2widgets_mapper[state_name] = item
+        self.state_list_widget.addItem(item)
+    
+    def remove_state(self, state_name):
+        if not state_name in self.state2widgets_mapper:
+            return
         
-    def keyReleaseEvent(self, e):
-        print('RELEAASS')
-        self.supervised_state_name = 'RELEAASE'
+        self.state_list_widget.removeItemWidget(self.state2widgets_mapper[state_name])
+        self.state2widgets_mapper.pop(state_name)
 
-    def buttonClicked(self):
-        print('OK')
+    # def keyPressEvent(self, e):
+
+    #     # # エスケープキーを押すと画面が閉じる
+    #     # if e.key() == Qt.Key_Escape:
+    #     #     self._widget.shutdown()
+        
+    #     print(e.key())
+    #     self.supervised_state_name = e.key()
+        
+    # def keyReleaseEvent(self, e):
+    #     print('RELEAASS')
+    #     self.supervised_state_name = 'RELEAASE'
--- src/rqt_stem_controller/src/rqt_stem_controller/stem_controller.py
+++ src/rqt_stem_controller/src/rqt_stem_controller/stem_controller.py
@@ -3,12 +3,16 @@ from python_qt_binding.QtWidgets import QVBoxLayout, QWidget
 from rqt_gui_py.plugin import Plugin
 from .controller_widget import ControllerWidget
 
+from rclpy.qos import QoSProfile
+from std_msgs.msg import String
+
 class STEMController(Plugin):
     def __init__(self, context):
         super(STEMController, self).__init__(context)
 
         self.setObjectName('STEMController')
 
+        self._node = context.node
         self._context = context
 
         self._widget = ControllerWidget()
@@ -16,9 +20,18 @@ class STEMController(Plugin):
             self._widget.setWindowTitle(
                 self._widget.windowTitle() + (' (%d)' % context.serial_number()))
         
-
         self._context.add_widget(self._widget)
+
+        self._publisher = self._node.create_publisher(String, 'supervised_state_name', qos_profile=QoSProfile(depth=10))
+        self._timer = self._node.create_timer(1.0, self.timer_callback)
+
+        self.state_list = ['not_touched', 'touched']
+
+        for state in self.state_list:
+            pass
     
+    def timer_callback(self):
+        print('test')
 
     def save_settings(self, plugin_settings, instance_settings):
         pass
