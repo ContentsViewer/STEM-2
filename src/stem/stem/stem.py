98602f9f0ff2cc49a39b01e043e6e2a1091193e6
IOE <fivetwothreesix@gmail.com>
2020-08-23 12:53:38 +0900

    developing sstem

--- src/stem/stem/stem.py
+++ src/stem/stem/stem.py
@@ -1,7 +1,11 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import QoSProfile
 
 from stem_interfaces.msg import GeneralSensorData
+from stem_interfaces.msg import SuperviseSignal
+
+import tensorflow as tf
 
 class STEM(Node):
     def __init__(self):
@@ -10,11 +14,25 @@ class STEM(Node):
             GeneralSensorData,
             'general_sensor_data',
             self.on_receive_sensor_data,
-            10)
+            QoSProfile(depth=10)
+        )
+        
+        self.supervise_signal_receiver = self.create_subscription(
+            SuperviseSignal,
+            'supervise_signal',
+            self.on_receiver_supervise_signal,
+            QoSProfile(depth=10)
+        )
+
         
     def on_receive_sensor_data(self, sensor_data):
         # self.get_logger().info(str(sensor_data.segments))
         pass
+    
+    def on_receiver_supervise_signal(self, supervise_signal):
+        # self.get_logger().info(supervise_signal.supervised_state_name)
+        pass
+
 
 def main(args=None):
     rclpy.init(args=args)
