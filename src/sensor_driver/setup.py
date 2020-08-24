5ede4f2d4ab25f58a8d4cc5e9c5882ac4434e406
IOE <fivetwothreesix@gmail.com>
2020-08-24 17:57:22 +0900

    add submodule stdlib

--- src/sensor_driver/setup.py
+++ src/sensor_driver/setup.py
@@ -5,7 +5,7 @@ package_name = 'sensor_driver'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name, package_name + '/lib'],
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
