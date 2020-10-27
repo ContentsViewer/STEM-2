74f614d5e63b91e965494e8d50c345e9533897fa
IOE <fivetwothreesix@gmail.com>
2020-10-27 18:32:36 +0900

    developing

--- src/rqt_stem_controller/src/rqt_stem_controller/controller_widget.py
+++ src/rqt_stem_controller/src/rqt_stem_controller/controller_widget.py
@@ -8,6 +8,32 @@ from python_qt_binding.QtGui import QIcon
 
 from qt_gui.ros_package_helper import get_package_path
 
+class SuperviseButton(QPushButton):
+    def __init__(self, state_name):
+        super(SuperviseButton, self).__init__(state_name)
+
+        self._state_name = state_name
+        self._is_pressed = False
+
+        self.pressed.connect(self._on_pressed)
+        self.released.connect(self._on_released)
+        
+    
+    @property
+    def state_name(self):
+        return self._state_name
+
+    @property
+    def is_pressed(self):
+        return self._is_pressed
+    
+    def _on_pressed(self):
+        self._is_pressed = True
+    
+    def _on_released(self):
+        self._is_pressed = False
+    
+    
 class ControllerWidget(QWidget):
     def __init__(self, parent=None):
         super(ControllerWidget, self).__init__(parent)
@@ -16,21 +42,20 @@ class ControllerWidget(QWidget):
         ui_file = os.path.join(package_path, 'share', 'rqt_stem_controller', 'resource', 'STEMController.ui')
         loadUi(ui_file, self)
 
-        # self.is_pressed_supervise_button = False
-        # self.supervise_button.pressed.connect(self.supervise_button_pressed)
-        # self.supervise_button.released.connect(self.supervise_button_released)
-        self.supervise_button_layout.addWidget(QPushButton('TEST'))
-        self.supervise_button_layout.addWidget(QPushButton('TEST2'))
-        self.supervise_button_layout.addWidget(QPushButton('TEST3'))
-        self.state_name_combo_box.setEditable(True)
-        self.state_name_remove_button.setIcon(QIcon.fromTheme('list-remove'))
-        self.state_name_remove_button.clicked.connect(self.remove_current_state)
+        self.supervise_buttons = []
+        # self.supervise_button_layout.addWidget(SuperviseButton('TEST'))
+        # self.supervise_button_layout.addWidget(SuperviseButton('TEST2'))
+        # self.supervise_button_layout.addWidget(SuperviseButton('TEST3'))
 
-    def supervise_button_pressed(self):
-        self.is_pressed_supervise_button = True
+        # self.state_name_combo_box.setEditable(True)
+        # self.state_name_remove_button.setIcon(QIcon.fromTheme('list-remove'))
+        # self.state_name_remove_button.clicked.connect(self.remove_current_state)
 
-    def supervise_button_released(self):
-        self.is_pressed_supervise_button = False
+    def set_supervise_buttons(self, state_names):
+        for state_name in state_names:
+            button = SuperviseButton(state_name)
+            self.supervise_buttons.append(button)
+            self.supervise_button_layout.addWidget(button)
 
     def remove_current_state(self):
         current_index = self.state_name_combo_box.currentIndex()
--- src/rqt_stem_controller/src/rqt_stem_controller/stem_controller.py
+++ src/rqt_stem_controller/src/rqt_stem_controller/stem_controller.py
@@ -1,9 +1,14 @@
 
 from python_qt_binding.QtWidgets import QVBoxLayout, QWidget
+from python_qt_binding.QtCore import Slot, QSignalMapper, QTimer, qWarning
 from rqt_gui_py.plugin import Plugin
 from .controller_widget import ControllerWidget
+from .controller_widget import SuperviseButton
 
 from rclpy.qos import QoSProfile
+from ros2param.api import call_get_parameters
+from rcl_interfaces.msg import ParameterType
+
 from stem_interfaces.msg import SuperviseSignal
 
 class STEMController(Plugin):
@@ -22,12 +27,43 @@ class STEMController(Plugin):
         
         self._context.add_widget(self._widget)
 
-        self._publisher = self._node.create_publisher(SuperviseSignal, 'supervise_signal', qos_profile=QoSProfile(depth=10))
-        self._timer = self._node.create_timer(0.02, self.timer_callback)
+        self._publisher = self._node.create_publisher(
+            SuperviseSignal, 
+            'supervise_signal', 
+            qos_profile=QoSProfile(depth=10)
+        )
+        # self._timer = self._node.create_timer(0.02, self.update)
+        
+        try:
+            response = call_get_parameters(node=self._node, node_name='stem', parameter_names=['state_names'])
+            pvalue = response.values[0]
+            if pvalue.type != ParameterType.PARAMETER_STRING_ARRAY:
+                raise RuntimeError('type of parameter "state_names" is Not STRING_ARRAY')
 
-    
-    def timer_callback(self):
-        pass
+            self.state_names = pvalue.string_array_value
+
+        except RuntimeError as e:
+            self._node.get_logger().warn(f'Failed to get parameter "stem/state_names". Please reload the plugin: {e}')
+
+        self._widget.set_supervise_buttons(self.state_names)
+        self._timer = QTimer(self)
+        self._timer.timeout.connect(self.update)
+        self._timer.start(20)
+        # print('ok')
+
+    def update(self):
+        supervised_state_name = 'none'
+        for supervise_button in self._widget.supervise_buttons:
+            if supervise_button.is_pressed:
+                supervised_state_name = supervise_button.state_name
+                break
+        # print('test')
+        supervise_signal = SuperviseSignal()
+        supervise_signal.supervised_state_name = supervised_state_name
+        self._publisher.publish(supervise_signal)
+
+        # print(self._widget.test_button.is_pressed)
+        
         # if self._widget.is_pressed_supervise_button:
         #     supervised_state_name = self._widget.state_name_combo_box.currentText()
         #     if supervised_state_name == '':
